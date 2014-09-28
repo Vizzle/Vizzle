4a8faa4a1ba25e6e23b68717804300f3001a2810
jayson <moxin.xt@taobao.com>
2014-09-28 22:24:33 +0800

    add workspace

---
+++ Example/VizzleListExample/VizzleListExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VizzleListExample
+//
+//  Created by moxin.xt on 14-9-28.
+//  Copyright (c) 2014年 VizLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
